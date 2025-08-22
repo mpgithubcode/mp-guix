15b5bbc615fdd272050dcf09bd346c7c435b77d9
averagedeveloper <mario.perry@protonmail.com>
2025-08-21 22:09:19 -0400

    Update nvidia-container-toolkit.scm

--- mp-guix-channel/nvidia/nvidia-container-toolkit.scm
+++ mp-guix-channel/nvidia/nvidia-container-toolkit.scm
@@ -78,7 +78,7 @@
                     (url "https://github.com/NVIDIA/libnvidia-container")
                     (commit (string-append "v" version))))
               (file-name (git-file-name name version))
-              (patches (search-patches "libnvidia-container.patch"))
+              (patches (local-file "libnvidia-container.patch")) 
               (sha256
                (base32 "0rzvh1zhh8pi5xjzaq3nmyzpcvjy41gq8w36dp1ai11a6j2lpa99"))))
     (build-system gnu-build-system)
