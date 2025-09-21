dc19e92cdeb998215a52317b12ee886163c2b3f8
averagedeveloper <mario.perry@protonmail.com>
2025-09-21 19:51:24 -0400

    Update btop-gpu.scm

--- mp-guix-channel/btop/btop-gpu.scm
+++ mp-guix-channel/btop/btop-gpu.scm
@@ -5,8 +5,6 @@
   #:use-module (guix build-system gnu)
   #:use-module (guix licenses)
   #:use-module (gnu packages ncurses)
-  #:use-module (gnu packages cmake)
-  #:use-module (gnu packages pkg-config)
   #:use-module (gnu packages bash)
   #:use-module (gnu packages admin)
   #:use-module (guix build utils)  ;; for nproc
@@ -20,13 +18,12 @@
     (version "1.4.4")
     (build-system gnu-build-system)
     (arguments
-     `(#:make-flags (list "GPU_SUPPORT=true")
-       #:phases
+     `(#:phases
        (modify-phases %standard-phases
-         ;; Remove configure phase since there is no configure script
+         ;; Remove configure phase
          (delete 'configure)
 
-         ;; Add NVIDIA library check before install
+         ;; Pre-install NVIDIA library check
          (add-before 'install 'check-nvidia-library
            (lambda* (#:key outputs #:allow-other-keys)
              (let ((nv-lib "/usr/lib/libnvidia-ml.so"))
@@ -35,4 +32,12 @@
                  (display-warning
                   'btop-gpu
                   "Warning: NVIDIA ML library not found. GPU monitoring may not work.")))
+             #t))
+
+         ;; Replace build phase to explicitly call make with GPU support
+         (replace 'build
+           (lambda* (#:key outputs #:allow-other-keys)
+             (invoke "make"
+                     (append (list "-j" (number->string (nproc)))
+                             (list "GPU_SUPPORT=true")))
              #t)))))))
