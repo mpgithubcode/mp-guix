a3d724f1c1aa00c6efa0bdccefd774eecae4af3f
averagedeveloper <mario.perry@protonmail.com>
2025-09-21 17:47:57 -0400

    removed btop and add admin for use modules

--- mp-guix-channel/btop/btop-gpu.scm
+++ mp-guix-channel/btop/btop-gpu.scm
@@ -8,7 +8,7 @@
   #:use-module (gnu packages cmake)
   #:use-module (gnu packages pkg-config)
   #:use-module (gnu packages bash)
-  #:use-module (guix packages btop)
+  #:use-module (guix packages admin)
   #:use-module (srfi srfi-1)
   #:use-module (gnu packages linux)) ; for lm-sensors
 
