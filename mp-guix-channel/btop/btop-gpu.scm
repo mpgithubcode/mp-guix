99d4a0b9fd10480d4bbfebba4fa283bf99aeae7d
averagedeveloper <mario.perry@protonmail.com>
2025-09-21 19:21:22 -0400

    Update btop-gpu.scm

--- mp-guix-channel/btop/btop-gpu.scm
+++ mp-guix-channel/btop/btop-gpu.scm
@@ -9,7 +9,7 @@
   #:use-module (gnu packages pkg-config)
   #:use-module (gnu packages bash)
   #:use-module (gnu packages admin)
-  #:use-module (guix build utils) ;; for nproc
+  #:use-module (guix build utils)  ;; for nproc
   #:use-module (srfi srfi-1)
   #:use-module (gnu packages linux))
 
@@ -21,28 +21,25 @@
     (arguments
      `(#:make-flags (list "GPU_SUPPORT=true")
        #:phases
-       (let ((phases %standard-phases))
-         ;; Remove the default check phase
-         (set! phases (filter (lambda (x) (not (eq? x 'check))) phases))
+       (modify-phases %standard-phases
+         ;; Remove the default 'check' phase
+         (delete 'check)
 
-         ;; Add build phase
-         (set! phases
-               (cons (lambda* (#:key outputs #:allow-other-keys)
-                       (invoke "make" (list "-j" (number->string (nproc))
-                                            "GPU_SUPPORT=true"))
-                       #t)
-                     phases))
+         ;; Pre-install NVIDIA library check
+         (add-before 'install 'check-nvidia-library
+           (lambda* (#:key outputs #:allow-other-keys)
+             (let ((nv-lib "/usr/lib/libnvidia-ml.so"))
+               (unless (or (file-exists? nv-lib)
+                           (file-exists? "/usr/lib64/libnvidia-ml.so"))
+                 (display-warning
+                  'btop-gpu
+                  "Warning: NVIDIA ML library not found. GPU monitoring may not work.")))
+             #t))
 
-         ;; Add NVIDIA pre-install check
-         (set! phases
-               (cons (lambda* (#:key outputs #:allow-other-keys)
-                       (let ((nv-lib "/usr/lib/libnvidia-ml.so"))
-                         (unless (or (file-exists? nv-lib)
-                                     (file-exists? "/usr/lib64/libnvidia-ml.so"))
-                           (display-warning
-                            'btop-gpu
-                            "Warning: NVIDIA ML library not found. GPU monitoring may not work.")))
-                       #t)
-                     phases))
-
-         phases)))))
+         ;; Replace the build phase to include GPU support
+         (replace 'build
+                  (lambda* (#:key outputs #:allow-other-keys)
+                    (invoke "make"
+                            (list "-j" (number->string (nproc))
+                                  "GPU_SUPPORT=true"))
+                    #t))))))))
