7d7372226296d4882cf570a83b6f8de2e21cc3c7
averagedeveloper <mario.perry@protonmail.com>
2025-09-21 19:08:20 -0400

    Update btop-gpu.scm

--- mp-guix-channel/btop/btop-gpu.scm
+++ mp-guix-channel/btop/btop-gpu.scm
@@ -21,20 +21,30 @@
     (arguments
      `(#:make-flags (list "GPU_SUPPORT=true")
        #:phases
-       (modify-phases %standard-phases
-         (delete 'check)
-         ;; Pre-install NVIDIA check
-         (add-before 'install 'check-nvidia-library
-           (lambda* (#:key outputs #:allow-other-keys)
-             (let ((nv-lib "/usr/lib/libnvidia-ml.so"))
-               (unless (or (file-exists? nv-lib)
-                           (file-exists? "/usr/lib64/libnvidia-ml.so"))
-                 (display-warning
-                  'btop-gpu
-                  "Warning: NVIDIA ML library not found. GPU monitoring may not work.")))
-             #t))
-         ;; Build phase
-         (add-before 'install 'build-btop-gpu
-           (lambda* (#:key outputs #:allow-other-keys)
-             (invoke "make" (list "-j" (number->string (nproc)) "GPU_SUPPORT=true"))
-             #t))))))))
+       (let ((phases %standard-phases))
+         ;; Remove the default check phase
+         (set! phases (delete 'check phases))
+
+         ;; Add a pre-install NVIDIA check
+         (set! phases
+               (add-before 'install 'check-nvidia-library
+                           (lambda* (#:key outputs #:allow-other-keys)
+                             (let ((nv-lib "/usr/lib/libnvidia-ml.so"))
+                               (unless (or (file-exists? nv-lib)
+                                           (file-exists? "/usr/lib64/libnvidia-ml.so"))
+                                 (display-warning
+                                  'btop-gpu
+                                  "Warning: NVIDIA ML library not found. GPU monitoring may not work.")))
+                             #t)
+                           phases))
+
+         ;; Add build phase before install
+         (set! phases
+               (add-before 'install 'build-btop-gpu
+                           (lambda* (#:key outputs #:allow-other-keys)
+                             (invoke "make" (list "-j" (number->string (nproc))
+                                                  "GPU_SUPPORT=true"))
+                             #t)
+                           phases))
+
+         phases)))))
